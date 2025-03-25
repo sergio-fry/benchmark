92fd74b491d431d21a08205be1ec9c30796def75
Sergei O. Udalov <udalov.x@mail.ru>
2025-03-25 08:19:22 +0300

    rails

---
+++ apps/rails/README.md
@@ -0,0 +1,47 @@
+# [Ruby on Rails](http://rubyonrails.org/) Benchmarking Test
+
+The information below contains information specific to Ruby on Rails.
+For further guidance, review the
+[documentation](https://github.com/TechEmpower/FrameworkBenchmarks/wiki).
+Also note the additional information provided in the [Ruby README](../).
+
+This is the Ruby on Rails portion of a [benchmarking test suite](../../)
+comparing a variety of web platforms.
+
+## Infrastructure Software Versions
+
+The tests were run with:
+
+- [Ruby 3.4](http://www.ruby-lang.org/)
+- [Rails 8.0](http://rubyonrails.org/)
+- [Puma 6.4](http://puma.io/)
+- [Iodine](https://github.com/boazsegev/iodine)
+- [Falcon](https://github.com/socketry/falcon)
+- [MySQL](https://dev.mysql.com/)
+- [PostgreSQL](https://www.postgresql.org/)
+- [Redis 6](https://redis.io)
+## Paths & Source for Tests
+
+- [JSON Serialization](app/controllers/hello_world_controller.rb): "/json"
+- [Single Database Query](app/controllers/hello_world_controller.rb): "/db", [World Model](app/models/world.rb)
+- [Multiple Database Queries](app/controllers/hello_world_controller.rb): "/queries?queries={#}", [World Model](app/models/world.rb)
+- [Cached Database Queries](app/controllers/hello_world_controller.rb): "/cached?queries={#}", [World Model]
+- [Fortunes](app/controllers/hello_world_controller.rb): "/fortune" , [Fortunes Model](app/models/fortune.rb)
+- [Database Updates](app/controllers/hello_world_controller.rb): "/update?queries={#}", [World Model](app/models/world.rb)
+- [Plaintext](app/controllers/hello_world_controller.rb): "/plaintext"
+
+## Get Help
+
+### Experts
+
+@spizm
+
+### Community
+
+- `#rubyonrails` IRC Channel ([irc.freenode.net](http://freenode.net/))
+- [Ruby on Rails Twitter](https://twitter.com/rails)
+- [Ruby on Rails Google Group](https://groups.google.com/forum/#!forum/rubyonrails-talk)
+
+### Resources
+
+- [Ruby on Rails Source Code](https://github.com/rails/rails)
---
+++ apps/rails/config/application.rb
@@ -0,0 +1,57 @@
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Hello
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults Rails::VERSION::STRING.to_f
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Only use headers required by TechEmpower.
+    config.action_dispatch.default_headers = {'Server' => 'Rails'}
+
+    config.api_only = true
+
+    config.middleware.delete ActionDispatch::Callbacks
+    config.middleware.delete ActionDispatch::DebugExceptions
+    config.middleware.delete ActionDispatch::Executor
+    config.middleware.delete ActionDispatch::RemoteIp
+    config.middleware.delete ActionDispatch::RequestId
+    config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ETag
+    config.middleware.delete Rack::Head
+    config.middleware.delete Rack::Runtime
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete Rails::Rack::Logger
+
+    config.active_support.isolation_level = :fiber if defined?(Falcon)
+
+    config.to_prepare do
+      HelloWorldController::ALL_IDS.each do |id|
+        Rails.cache.write("world_#{id}", World.find(id).as_json, expires_in: 1.day)
+      end
+    end
+  end
+end
