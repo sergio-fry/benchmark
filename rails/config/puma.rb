e88b253b257632e30fa93b84d1f9828972c341b3
Sergei O. Udalov <udalov.x@mail.ru>
2025-04-15 21:20:34 +0300

    disable autotune

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -2,7 +2,10 @@
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 
-require_relative 'auto_tune'
+# отключаем, чтобы не дать преимущество MRI, 
+# так как пока не готов делать такую же оптимизацию
+# для JVM/Truffle 
+# require_relative 'auto_tune'
 
 # Puma starts a configurable number of processes (workers) and each process
 # serves each request in a thread from an internal thread pool.
