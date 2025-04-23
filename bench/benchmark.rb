0fc6af05cf82a92d75e8600c3dc0bf39411a67b4
Sergei O. Udalov <udalov.x@mail.ru>
2025-04-23 17:07:49 +0300

    docker limits

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -145,9 +145,10 @@ accuracity = ENV.fetch("ACCURACITY", "0.9").to_f
 logger.info "Target accuracity = #{accuracity}"
 
 format = ENV.fetch("FORMAT", "text")
+throughput = Throughput.new(ARGV[0], accuracity:, logger:)
 case format
 when "text"
-  puts Throughput.new(ARGV[0], accuracity:, logger:).rps
+  puts "URL='#{ARGV[0]}', accuracity=#{accuracity}, rps = #{throughput.rps}, concurrency = #{throughput.concurrency}"
 when "json"
   throughput = Throughput.new(ARGV[0], accuracity:, logger:)
   puts({ url: ARGV[0], accuracity:, rps: throughput.rps, concurrency: throughput.concurrency }.to_json)
